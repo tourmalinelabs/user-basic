c4cef3a463946584f434355b059b0650530eed9a
alpertl <87487862+alpertl@users.noreply.github.com>
2021-08-11 14:24:05 +0200

    use basic prettier

--- gruntfile.js
+++ gruntfile.js
@@ -3,53 +3,55 @@
 const del = require('del');
 
 module.exports = function (grunt) {
-    grunt.initConfig({
-        mochaTest: {
-            unit: {
-                src: ['build/**/*.unit.js', '!build/notification-model-email.unit.js']
-            },
-            mail: {
-                options: { timeout: 10000 },
-                src: ['build/notification-model-email.unit.js']
-            }
-        },
-        watch: {
-            scripts: {
-                files: ['**/*'],
-                tasks: ['test'],
-                options: { spawn: true }
-            }
-        },
-        ts: {
-            default: {
-                tsconfig: './tsconfig.json',
-                src: ['./src/**/*.ts', '!node_modules/**'],
-                outDir: './build'
-            }
-        },
-        copy: {
-            main: {
-                files: [{ 
-                    expand: true, 
-                    cwd: 'src', 
-                    src: ['**/*.json', '!node_modules/**'], 
-                    dest: './build' 
-                }],
-            }
-        }
-    });
+  grunt.initConfig({
+    mochaTest: {
+      unit: {
+        src: ['build/**/*.unit.js', '!build/notification-model-email.unit.js'],
+      },
+      mail: {
+        options: { timeout: 10000 },
+        src: ['build/notification-model-email.unit.js'],
+      },
+    },
+    watch: {
+      scripts: {
+        files: ['**/*'],
+        tasks: ['test'],
+        options: { spawn: true },
+      },
+    },
+    ts: {
+      default: {
+        tsconfig: './tsconfig.json',
+        src: ['./src/**/*.ts', '!node_modules/**'],
+        outDir: './build',
+      },
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['**/*.json', '!node_modules/**'],
+            dest: './build',
+          },
+        ],
+      },
+    },
+  });
 
-    grunt.loadNpmTasks("grunt-ts");
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-ts');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('clean-build', function () {
-        const done = this.async();
-        del(['build/**/*']).then(() => done());
-    });
+  grunt.registerTask('clean-build', function () {
+    const done = this.async();
+    del(['build/**/*']).then(() => done());
+  });
 
-    grunt.registerTask('build', ['clean-build', 'copy', 'ts']);
-    grunt.registerTask('test', ['build', 'mochaTest:unit']);
-    grunt.registerTask('test-mail', ['build', 'mochaTest:mail']);
+  grunt.registerTask('build', ['clean-build', 'copy', 'ts']);
+  grunt.registerTask('test', ['build', 'mochaTest:unit']);
+  grunt.registerTask('test-mail', ['build', 'mochaTest:mail']);
 };
